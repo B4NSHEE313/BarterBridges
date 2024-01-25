0c919fde78f37a5dd75c5f70693b2d417ae8ae32
David Hackel <Sidewalksleepin@gmail.com>
2024-01-25 09:47:01 -0500

    Initial Commit

---
+++ BarterBridges/BarterBridgesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarterBridgesApp.swift
+//  BarterBridges
+//
+//  Created by David David on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BarterBridgesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
